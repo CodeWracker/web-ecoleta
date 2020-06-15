e34cd8f9e268fcd5238ae1076b76a88237a5d198
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-06-14 23:06:33 -0300

    arrumei o nome da env var

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-
 const api = axios.create({
-  baseURL: process.env.MY_API_PATH || "http://localhost:3333",
+  baseURL: process.env.REACT_APP_API_PATH || "http://localhost:3333",
 });
 export default api;
