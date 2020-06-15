3a6095517e990ea6f99fa4254fd3400b9e8cc2f1
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-06-14 22:52:32 -0300

    add .env path

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: process.env.MY_API_PATH || "http://localhost:3333",
 });
 export default api;
